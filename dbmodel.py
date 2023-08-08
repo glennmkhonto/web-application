25215e518c4fa98c33505dc1917cea1c0bf102a1
Glenn Mkhonto <glenn.mkhonto@dimensiondata.com>
2023-08-08 23:10:26 +0200

    Removing unused code and adding packages in the requirements text file

--- dbmodel.py
+++ dbmodel.py
@@ -1,19 +1,6 @@
 from db import db
 from sqlalchemy import String, TIMESTAMP, DECIMAL, Integer
 
-#db = SQLAlchemy()
-
-# class Data(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100))
-#     email = db.Column(db.String(100))
-#     phone = db.Column(db.String(100))
-
-#     def __init__(self, name, email, phone):
-#         self.name = name
-#         self.email = email
-#         self.phone = phone
-
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vehicleid = db.Column(String(10))
@@ -23,9 +10,9 @@ class Data(db.Model):
     event_description = db.Column(String(255))
     speed = db.Column(Integer)
     heading = db.Column(Integer)
-    ignitionState = db.Column(String(3))  # Adjust type as needed
+    ignitionState = db.Column(String(3))  
     gps_accuracy = db.Column(Integer)
-    gps_fix_type = db.Column(Integer)  # Adjust type as needed
+    gps_fix_type = db.Column(Integer)  
 
 
 
